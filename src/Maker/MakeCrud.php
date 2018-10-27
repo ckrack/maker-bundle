3442b124301891a0cf9d985a17333b55642e3190
Clemens Krack <info@clemenskrack.com>
2018-10-28 00:34:49 +0200

    Tabs to Spaces

--- src/Maker/MakeCrud.php
+++ src/Maker/MakeCrud.php
@@ -176,8 +176,8 @@ final class MakeCrud extends AbstractMaker
                 'route_path' => Str::asRoutePath($controllerClassDetails->getRelativeNameWithoutSuffix()),
                 'route_name' => $routeName,
                 'class_name' => Str::getShortClassName($testClassDetails->getFullName()),
-				'namespace' => Str::getNamespace($testClassDetails->getFullName()),
-				'form_fields' => $entityDoctrineDetails->getFormFields(),
+                'namespace' => Str::getNamespace($testClassDetails->getFullName()),
+                'form_fields' => $entityDoctrineDetails->getFormFields(),
             ]
         );
 
